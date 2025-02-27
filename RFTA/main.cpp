cc2ef4dd5820dda12e18d69389f7422fa5b8d70f
rotat1ck <rotatick32151@gmail.com>
2025-02-27 11:20:36 +0300

    updated RFTA project name

--- RFTA/main.cpp
+++ RFTA/main.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    int fontId = QFontDatabase::addApplicationFont(":/fonts/Inter-VariableFont_opsz,wght.ttf");
+    QFontDatabase::addApplicationFont(":/fonts/Inter-VariableFont_opsz,wght.ttf");
     MainWindow w;
     w.setFixedSize(QSize(750, 500));;
     w.show();
